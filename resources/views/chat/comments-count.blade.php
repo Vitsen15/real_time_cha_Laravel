4e0672703c27d7342f1cf383f0f90f791cf9a67f
Andrew Vitsentovich <vav@tangramltd.com>
2018-03-13 12:41:23 +0200

    move all content to section and remove files of section content

--- resources/views/chat/comments-count.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="text-right">
-    <b>Massages count: {{{0}}}</b>
-    <br>
-</div>
--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -3,8 +3,29 @@
 @section('content')
     <div class="container">
         <h2 class="text-center">{{ $title }}</h2>
-        @include('chat._form')
-        @include('chat.comments-count')
-        @include('chat.comment')
+
+        <form class="form-horizontal col align-self-center" method="post">
+            <div class="form-group">
+                <label for="comment">Message</label>
+                <textarea class="form-control" rows="3" id="comment"></textarea>
+            </div>
+            <button type="submit" class="btn btn-default">Send</button>
+        </form>
+
+        <div class="text-right">
+            <b>Massages count: {{ $messagesCount }}</b>
+            <br>
+        </div>
+
+        @foreach($messages as $message)
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{ $message->user }}</h3>
+                    <p class="text-left">{{ $message->user }}</p>
+                    <p class="text-right">{{ $message->created_at }}</p>
+                </div>
+                <div class="panel-body">{{ $message->message }}</div>
+            </div>
+        @endforeach
     </div>
-@stop
+@endsection
