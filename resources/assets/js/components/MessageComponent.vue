1d3dafd6d3edc8a1fbb0f9a43bacfcc6dc571a43
Andrew Vitsentovich <vav@tangramltd.com>
2018-03-14 18:31:07 +0200

    build front-end

---
+++ resources/assets/js/components/MessageComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card bg-info text-white">
+        <div class="card-header">
+            <h3>{{ messages.user.name }}</h3>
+            <p class="text-right">{{ messages.created_at }}</p>
+        </div>
+        <div class="card-body">
+            <p>{{ messages.message }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['messages'],
+        mounted() {
+            console.log(this.messages)
+        }
+    }
+</script>
---
+++ resources/assets/js/components/SendMessageComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <form class="form-horizontal col align-self-center" @submit.prevent.keyup="sent">
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea class="form-control" rows="3" id="message" v-model="message.message" placeholder="Enter the message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-default">Send</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        props: ['user'],
+        data() {
+            return {
+                message: {
+                    message: '',
+                    user: this.user
+                }
+            }
+        },
+        methods: {
+            sent() {
+                this.$emit('messagesent', this.message);
+                this.message = {};
+            }
+        }
+    }
+</script>
