2ba33f8a955406617ed5d8bca2492ee1e081a2b5
Andrew Vitsentovich <vav@tangramltd.com>
2018-03-14 17:11:19 +0200

    add pusher server to application

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.7 on 2018-03-06 12:03:48.
+ * Generated for Laravel 5.6.7 on 2018-03-14 15:09:27.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -9713,229 +9713,229 @@ namespace Illuminate\Support\Facades {
          
     }
 
-//    class Schema {
-//
-//        /**
-//         * Determine if the given table exists.
-//         *
-//         * @param string $table
-//         * @return bool
-//         * @static
-//         */
-//        public static function hasTable($table)
-//        {
-//            return \Illuminate\Database\Schema\MySqlBuilder::hasTable($table);
-//        }
-//
-//        /**
-//         * Get the column listing for a given table.
-//         *
-//         * @param string $table
-//         * @return array
-//         * @static
-//         */
-//        public static function getColumnListing($table)
-//        {
-//            return \Illuminate\Database\Schema\MySqlBuilder::getColumnListing($table);
-//        }
-//
-//        /**
-//         * Drop all tables from the database.
-//         *
-//         * @return void
-//         * @static
-//         */
-//        public static function dropAllTables()
-//        {
-//            \Illuminate\Database\Schema\MySqlBuilder::dropAllTables();
-//        }
-//
-//        /**
-//         * Set the default string length for migrations.
-//         *
-//         * @param int $length
-//         * @return void
-//         * @static
-//         */
-//        public static function defaultStringLength($length)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::defaultStringLength($length);
-//        }
-//
-//        /**
-//         * Determine if the given table has a given column.
-//         *
-//         * @param string $table
-//         * @param string $column
-//         * @return bool
-//         * @static
-//         */
-//        public static function hasColumn($table, $column)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::hasColumn($table, $column);
-//        }
-//
-//        /**
-//         * Determine if the given table has given columns.
-//         *
-//         * @param string $table
-//         * @param array $columns
-//         * @return bool
-//         * @static
-//         */
-//        public static function hasColumns($table, $columns)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::hasColumns($table, $columns);
-//        }
-//
-//        /**
-//         * Get the data type for the given column name.
-//         *
-//         * @param string $table
-//         * @param string $column
-//         * @return string
-//         * @static
-//         */
-//        public static function getColumnType($table, $column)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::getColumnType($table, $column);
-//        }
-//
-//        /**
-//         * Modify a table on the schema.
-//         *
-//         * @param string $table
-//         * @param \Closure $callback
-//         * @return void
-//         * @static
-//         */
-//        public static function table($table, $callback)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::table($table, $callback);
-//        }
-//
-//        /**
-//         * Create a new table on the schema.
-//         *
-//         * @param string $table
-//         * @param \Closure $callback
-//         * @return void
-//         * @static
-//         */
-//        public static function create($table, $callback)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::create($table, $callback);
-//        }
-//
-//        /**
-//         * Drop a table from the schema.
-//         *
-//         * @param string $table
-//         * @return void
-//         * @static
-//         */
-//        public static function drop($table)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::drop($table);
-//        }
-//
-//        /**
-//         * Drop a table from the schema if it exists.
-//         *
-//         * @param string $table
-//         * @return void
-//         * @static
-//         */
-//        public static function dropIfExists($table)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::dropIfExists($table);
-//        }
-//
-//        /**
-//         * Rename a table on the schema.
-//         *
-//         * @param string $from
-//         * @param string $to
-//         * @return void
-//         * @static
-//         */
-//        public static function rename($from, $to)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::rename($from, $to);
-//        }
-//
-//        /**
-//         * Enable foreign key constraints.
-//         *
-//         * @return bool
-//         * @static
-//         */
-//        public static function enableForeignKeyConstraints()
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::enableForeignKeyConstraints();
-//        }
-//
-//        /**
-//         * Disable foreign key constraints.
-//         *
-//         * @return bool
-//         * @static
-//         */
-//        public static function disableForeignKeyConstraints()
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::disableForeignKeyConstraints();
-//        }
-//
-//        /**
-//         * Get the database connection instance.
-//         *
-//         * @return \Illuminate\Database\Connection
-//         * @static
-//         */
-//        public static function getConnection()
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::getConnection();
-//        }
-//
-//        /**
-//         * Set the database connection instance.
-//         *
-//         * @param \Illuminate\Database\Connection $connection
-//         * @return $this
-//         * @static
-//         */
-//        public static function setConnection($connection)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            return \Illuminate\Database\Schema\MySqlBuilder::setConnection($connection);
-//        }
-//
-//        /**
-//         * Set the Schema Blueprint resolver callback.
-//         *
-//         * @param \Closure $resolver
-//         * @return void
-//         * @static
-//         */
-//        public static function blueprintResolver($resolver)
-//        {
-//            //Method inherited from \Illuminate\Database\Schema\Builder
-//            \Illuminate\Database\Schema\MySqlBuilder::blueprintResolver($resolver);
-//        }
-//
-//    }
+    class Schema {
+        
+        /**
+         * Determine if the given table exists.
+         *
+         * @param string $table
+         * @return bool 
+         * @static 
+         */ 
+        public static function hasTable($table)
+        {
+            return \Illuminate\Database\Schema\MySqlBuilder::hasTable($table);
+        }
+        
+        /**
+         * Get the column listing for a given table.
+         *
+         * @param string $table
+         * @return array 
+         * @static 
+         */ 
+        public static function getColumnListing($table)
+        {
+            return \Illuminate\Database\Schema\MySqlBuilder::getColumnListing($table);
+        }
+        
+        /**
+         * Drop all tables from the database.
+         *
+         * @return void 
+         * @static 
+         */ 
+        public static function dropAllTables()
+        {
+            \Illuminate\Database\Schema\MySqlBuilder::dropAllTables();
+        }
+        
+        /**
+         * Set the default string length for migrations.
+         *
+         * @param int $length
+         * @return void 
+         * @static 
+         */ 
+        public static function defaultStringLength($length)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::defaultStringLength($length);
+        }
+        
+        /**
+         * Determine if the given table has a given column.
+         *
+         * @param string $table
+         * @param string $column
+         * @return bool 
+         * @static 
+         */ 
+        public static function hasColumn($table, $column)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::hasColumn($table, $column);
+        }
+        
+        /**
+         * Determine if the given table has given columns.
+         *
+         * @param string $table
+         * @param array $columns
+         * @return bool 
+         * @static 
+         */ 
+        public static function hasColumns($table, $columns)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::hasColumns($table, $columns);
+        }
+        
+        /**
+         * Get the data type for the given column name.
+         *
+         * @param string $table
+         * @param string $column
+         * @return string 
+         * @static 
+         */ 
+        public static function getColumnType($table, $column)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::getColumnType($table, $column);
+        }
+        
+        /**
+         * Modify a table on the schema.
+         *
+         * @param string $table
+         * @param \Closure $callback
+         * @return void 
+         * @static 
+         */ 
+        public static function table($table, $callback)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::table($table, $callback);
+        }
+        
+        /**
+         * Create a new table on the schema.
+         *
+         * @param string $table
+         * @param \Closure $callback
+         * @return void 
+         * @static 
+         */ 
+        public static function create($table, $callback)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::create($table, $callback);
+        }
+        
+        /**
+         * Drop a table from the schema.
+         *
+         * @param string $table
+         * @return void 
+         * @static 
+         */ 
+        public static function drop($table)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::drop($table);
+        }
+        
+        /**
+         * Drop a table from the schema if it exists.
+         *
+         * @param string $table
+         * @return void 
+         * @static 
+         */ 
+        public static function dropIfExists($table)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::dropIfExists($table);
+        }
+        
+        /**
+         * Rename a table on the schema.
+         *
+         * @param string $from
+         * @param string $to
+         * @return void 
+         * @static 
+         */ 
+        public static function rename($from, $to)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::rename($from, $to);
+        }
+        
+        /**
+         * Enable foreign key constraints.
+         *
+         * @return bool 
+         * @static 
+         */ 
+        public static function enableForeignKeyConstraints()
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::enableForeignKeyConstraints();
+        }
+        
+        /**
+         * Disable foreign key constraints.
+         *
+         * @return bool 
+         * @static 
+         */ 
+        public static function disableForeignKeyConstraints()
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::disableForeignKeyConstraints();
+        }
+        
+        /**
+         * Get the database connection instance.
+         *
+         * @return \Illuminate\Database\Connection 
+         * @static 
+         */ 
+        public static function getConnection()
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::getConnection();
+        }
+        
+        /**
+         * Set the database connection instance.
+         *
+         * @param \Illuminate\Database\Connection $connection
+         * @return $this 
+         * @static 
+         */ 
+        public static function setConnection($connection)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            return \Illuminate\Database\Schema\MySqlBuilder::setConnection($connection);
+        }
+        
+        /**
+         * Set the Schema Blueprint resolver callback.
+         *
+         * @param \Closure $resolver
+         * @return void 
+         * @static 
+         */ 
+        public static function blueprintResolver($resolver)
+        {
+            //Method inherited from \Illuminate\Database\Schema\Builder            
+            \Illuminate\Database\Schema\MySqlBuilder::blueprintResolver($resolver);
+        }
+         
+    }
 
     class Session {
         
