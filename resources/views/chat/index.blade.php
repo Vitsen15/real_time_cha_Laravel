0ca12309ff72f243a0561a0cb4e2d034c8c22885
Andrew Vitsentovich <vav@tangramltd.com>
2018-03-27 15:19:43 +0300

    remove content to view.js components

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -2,45 +2,7 @@
 
 @section('content')
     <div class="container">
-        {{--<h2 class="text-center">{{ $title }}</h2>--}}
-        {{--<form class="form-horizontal col align-self-center" action="{{ route('send-message') }}" method="POST">--}}
-            {{--@csrf--}}
-            {{--<div class="form-group">--}}
-                {{--<label for="message">Message</label>--}}
-                {{--<textarea class="form-control" rows="3" id="message" name="message"></textarea>--}}
-            {{--</div>--}}
-            {{--<button type="submit" class="btn btn-default">Send</button>--}}
-        {{--</form>--}}
-
-        {{--<div class="text-right">--}}
-            {{--<b>Massages count: {{ $messagesCount }}</b>--}}
-            {{--<br>--}}
-        {{--</div>--}}
-        <message :messages="messages"></message>
         <sent-message v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></sent-message>
-        {{--@foreach($messages as $message)--}}
-            {{--@if($message->user->id === Auth::user()->id)--}}
-                {{--<div class="card bg-info text-white">--}}
-                    {{--<div class="card-header">--}}
-                        {{--<h3>{{ $message->user->name }}</h3>--}}
-                        {{--<p class="text-right">{{ $message->created_at }}</p>--}}
-                    {{--</div>--}}
-                    {{--<div class="card-body">{{ $message->message }}</div>--}}
-                {{--</div>--}}
-            {{--@else--}}
-                {{--<div class="card">--}}
-                    {{--<div class="card-header">--}}
-                        {{--<h3>{{ $message->user->name }}</h3>--}}
-                        {{--<p class="text-right">{{ $message->created_at }}</p>--}}
-                    {{--</div>--}}
-                    {{--<div class="card-body">{{ $message->message }}</div>--}}
-                {{--</div>--}}
-            {{--@endif--}}
-        {{--@endforeach--}}
-        <br>
-
-        {{--<div class="text-center">--}}
-            {{--{!! $messages->render() !!}--}}
-        {{--</div>--}}
+        <message :messages="messages"></message>
     </div>
 @endsection
